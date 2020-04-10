f69b28c9eff26074ee7d8c814e8e94afef6d05eb
Ivan Markin <53366294+ivan-markin@users.noreply.github.com>
2020-04-10 18:19:56 +0300

    Updates

--- src/analytics/index.js
+++
@@ -1,6 +0,0 @@
-import './analytics.css';
-import '../blocks/logo/logo.css';
-import '../blocks/content-title/content-title.css';
-import '../blocks/content-small-title/content-small-title.css';
-import '../blocks/content-paragraph-text/content-paragraph-text.css';
-import '../blocks/footer/footer.css';
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import './style.css';
-import './analytics/analytics.css';
-import './about/about.css';
